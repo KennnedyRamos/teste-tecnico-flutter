86a5292bca65493341e233ab18f6a9e5c0e9f01e
KennedyRamos <kennedy_ramos9@icloud.com>
2023-12-08 20:59:18 -0300

    Readmi novo

---
+++ teste_tecnico1/bin/teste_tecnico1.dart
@@ -0,0 +1,5 @@
+import 'package:teste_tecnico1/teste_tecnico1.dart' as teste_tecnico1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${teste_tecnico1.calculate()}!');
+}
---
+++ teste_tecnico1/test/teste_tecnico1_test.dart
@@ -0,0 +1,8 @@
+import 'package:teste_tecnico1/teste_tecnico1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
